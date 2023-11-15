9c04bc0766c9284740e12dd09d3f22a4df30a88b
packkill <81907020+packkill@users.noreply.github.com>
2023-11-15 23:38:26 +0900

    add Map class

---
+++ app/src/main/java/uos/teamkernel/sim/Map.java
@@ -0,0 +1,52 @@
+package uos.teamkernel.sim;
+
+import uos.teamkernel.common.Point;
+import java.util.ArrayList;
+import uos.teamkernel.common.Spot;
+import uos.teamkernel.model.MapModel;
+import uos.teamkernel.model.ModelObserver;
+
+public class Map implements MapModel {
+
+    private Spot[][] map;
+    private ArrayList<ModelObserver> observers;
+
+    public Map() {
+        map = new Spot[10][10];
+        observers = new ArrayList<ModelObserver>();
+    }
+
+    public Spot getSpot(Point position) {
+        return map[position.getX()][position.getY()];
+    }
+
+    public Spot getSpot(int x, int y) {
+        return map[x][y];
+    }
+
+    public void setSpot(Point position, Spot spot) {
+        map[position.getX()][position.getY()] = spot;
+        notifyObservers(); // notify observers that the state of model has changed
+    }
+
+    public int getWidth() {
+        return map.length;
+    }
+
+    public int getHeight() {
+        return map[0].length;
+    }
+
+    /* below is for observer pattern */
+
+    private void notifyObservers() {
+        for (ModelObserver observer : observers) {
+            observer.modelChanged(this);
+        }
+    }
+
+    public void addObserver(ModelObserver observer) {
+        observers.add(observer);
+    }
+
+}
