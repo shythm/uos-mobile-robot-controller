0560eb46ff0c24c93032ae8443c63a97686ce288
Seongho Lee <tjdgh2626@gmail.com>
2023-11-20 04:39:58 +0900

    Update SimMainView

--- app/src/main/java/uos/teamkernel/sim/App.java
+++ app/src/main/java/uos/teamkernel/sim/App.java
@@ -1,15 +1,43 @@
 package uos.teamkernel.sim;
 
+import uos.teamkernel.common.Spot;
+import uos.teamkernel.common.Point;
+
+import uos.teamkernel.model.MapModel;
+import uos.teamkernel.model.MobileRobotModel;
+
 import uos.teamkernel.view.InitDialogForm;
+import uos.teamkernel.view.SimMainView;
 
 public class App {
     public static void main(String[] args) {
         // Ask for input to initialize
-        new InitDialogForm(Thread.currentThread());
+        InitDialogForm initDialogForm = new InitDialogForm(Thread.currentThread());
         try {
             Thread.sleep(Long.MAX_VALUE);
         } catch (InterruptedException e) {
             System.out.println("Initialize done. Start Simulator.");
         }
+
+        int mapWidth = (int)initDialogForm.getMapSize().getWidth();
+        int mapHeight = (int)initDialogForm.getMapSize().getHeight();
+
+        // Initialize map
+        MapModel realMap = new Map(mapWidth, mapHeight);
+        for (Point p : initDialogForm.getHazardPoint()) { // set hazard spot
+            realMap.setSpot(p, Spot.HAZARD);
+        }
+        for (Point p : initDialogForm.getColorPoint()) { // set color blob spot
+            realMap.setSpot(p, Spot.COLOR_BLOB);
+        }
+        for (Point p : initDialogForm.getDestPoint()) { // set destination spot
+            realMap.setSpot(p, Spot.PREDEFINED_SPOT);
+        }
+
+        // Initialize robot
+        MapModel robotMap = new Map(mapWidth, mapHeight);
+        MobileRobotModel robot = new MobileRobot();
+
+        new SimMainView(robotMap, robot);
     }
 }
--- app/src/main/java/uos/teamkernel/view/SimMainView.java
+++ app/src/main/java/uos/teamkernel/view/SimMainView.java
@@ -1,32 +1,85 @@
 package uos.teamkernel.view;
 
-import javax.swing.*;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.FlowLayout;
+import java.awt.Font;
 import java.awt.event.ActionListener;
+
+import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import uos.teamkernel.model.MapModel;
+import uos.teamkernel.model.MobileRobotModel;
 import uos.teamkernel.model.ModelObserver;
 
-public abstract class SimMainView extends JFrame implements ModelObserver {
+public class SimMainView extends JFrame implements ModelObserver {
+    // there is two button in this view for step and voice
+    private JButton stepButton;
+    private JButton voiceButton;
+    private MapPanelView mapPanelView;
 
-    /**
-     * Constructor
-     * 
-     * @param title the title of the view
-     */
-    public SimMainView(String title) {
-        super(title);
+    public SimMainView(MapModel mapModel, MobileRobotModel robotModel) {
+        super("MainView");
+
+        JLabel mapLabel = new JLabel("Map");
+        mapLabel.setFont(new Font("Broadway", Font.PLAIN, 30));
+        mapLabel.setAlignmentX(Component.LEFT_ALIGNMENT);
+        mapLabel.setBorder(BorderFactory.createEmptyBorder(0, 20, 0, 0));
+
+        mapPanelView = new MapPanelView(mapModel, robotModel);
+        mapPanelView.setAlignmentX(Component.LEFT_ALIGNMENT);
+        mapPanelView.setBackground(Color.WHITE);
+        mapPanelView.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+
+        JPanel mapPanel = new JPanel();
+        mapPanel.setLayout(new BoxLayout(mapPanel, BoxLayout.Y_AXIS));
+        mapPanel.add(mapLabel);
+        mapPanel.add(mapPanelView);
+
+        JButton stepButton = new JButton("Step");
+        JButton voiceButton = new JButton("Voice");
+
+        JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        buttonPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+        buttonPanel.setAlignmentX(Component.LEFT_ALIGNMENT);
+        buttonPanel.add(stepButton);
+        buttonPanel.add(voiceButton);
+
+        JPanel mainPanel = new JPanel();
+        mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
+        mainPanel.setAlignmentX(Component.LEFT_ALIGNMENT);
+        mainPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+        mainPanel.add(mapPanel);
+        mainPanel.add(buttonPanel);
+
+        add(mainPanel);
+        pack();
+        setResizable(false);
+        setVisible(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
-    /**
-     * add step button listener
-     * 
-     * @param listener the handler of step button event
-     */
-    public abstract void addStepButtonListener(ActionListener listener);
+    public void modelChanged(Object model) {
+        /**
+         * This method is called when the map or mobile robot has changed
+         */
+        if (model instanceof MapModel) {
+        } else if (model instanceof MobileRobotModel) {
+        }
 
-    /**
-     * add voice button listener
-     * 
-     * @param listener the handler of voice button event
-     */
-    public abstract void addVoiceButtonListener(ActionListener listener);
+        mapPanelView.repaint();
+    }
 
+    public void addStepButtonListener(ActionListener listener) {
+        stepButton.addActionListener(listener);
+    }
+
+    public void addVoiceButtonListener(ActionListener listener) {
+        voiceButton.addActionListener(listener);
+    }
 }
