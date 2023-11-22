90b1bb966b32af97903786ca565c9832c91f6021
DrCloy <alsry0712@gmail.com>
2023-11-22 16:42:45 +0900

    Error fix of %20 to space

--- app/src/main/java/uos/teamkernel/view/MapPanelView.java
+++ app/src/main/java/uos/teamkernel/view/MapPanelView.java
@@ -2,7 +2,7 @@ package uos.teamkernel.view;
 
 import java.io.File;
 import java.io.IOException;
-
+import java.net.URLDecoder;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 
@@ -55,7 +55,8 @@ public class MapPanelView extends JPanel {
         Image ret = null;
 
         try {
-            BufferedImage bi = ImageIO.read(new File(getClass().getResource(path).getPath()));
+            String fixedPath = URLDecoder.decode(getClass().getResource(path).getPath(), "UTF-8");
+            BufferedImage bi = ImageIO.read(new File(fixedPath));
             ret = bi.getScaledInstance(distance, distance, Image.SCALE_SMOOTH);
         } catch (IOException e) {
             System.out.println("Image " + path + " not found");
