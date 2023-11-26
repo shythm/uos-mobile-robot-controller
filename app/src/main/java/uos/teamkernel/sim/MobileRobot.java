02aa902eda57734012fcc5e486e5b20934bd0970
Seongho Lee <tjdgh2626@gmail.com>
2023-11-26 21:01:27 +0900

    Update y-coordinate and add check moving position validation

--- app/src/main/java/uos/teamkernel/sim/MobileRobot.java
+++ app/src/main/java/uos/teamkernel/sim/MobileRobot.java
@@ -35,9 +35,9 @@ public class MobileRobot implements MobileRobotModel {
 
     public Point predictNextPosition(Direction d) {
         Point nextPosition = switch (d) {
-        case NORTH -> new Point(position.x, position.y - 1);
+        case NORTH -> new Point(position.x, position.y + 1);
         case EAST -> new Point(position.x + 1, position.y);
-        case SOUTH -> new Point(position.x, position.y + 1);
+        case SOUTH -> new Point(position.x, position.y - 1);
         case WEST -> new Point(position.x - 1, position.y);
         default -> new Point(-1, -1);
         };
@@ -46,7 +46,16 @@ public class MobileRobot implements MobileRobotModel {
     }
 
     public Point move() {
-        position = predictNextPosition(direction);
+        // predict next position
+        Point ret = predictNextPosition(direction);
+
+        // check if the position is valid
+        if (!isInsideMap(ret)) {
+            System.out.println("move: Invalid Position");
+            ret = this.position;
+        }
+
+        position = ret;
         notifyObservers(); // notify observers that the state of model has changed
         return position;
     }
